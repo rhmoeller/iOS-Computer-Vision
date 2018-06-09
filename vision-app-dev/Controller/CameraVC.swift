8f797117abf1fff7bf061127ab286a99167a7341
rhmoeller <robert@hjortsholm.com>
2018-06-09 22:44:56 +0100

    Initial Commit

---
+++ vision-app-dev/Controller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  CameraVC.swift
+//  vision-app-dev
+//
+//  Created by Robert Møller on 09/06/2018.
+//  Copyright © 2018 Robert Møller. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
